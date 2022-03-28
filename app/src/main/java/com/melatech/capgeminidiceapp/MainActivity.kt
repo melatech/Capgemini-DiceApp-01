6d128ddbb7997623d92d7af66ee54f14cb87be83
melatech <maincontractor@gmail.com>
2022-03-28 11:58:13 +0100

    second commit-implement newDiceRoll function

--- app/src/main/java/com/melatech/capgeminidiceapp/MainActivity.kt
+++ app/src/main/java/com/melatech/capgeminidiceapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.melatech.capgeminidiceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun newDiceRoll(v: View){
+
+        var countersArr = arrayOf(-1, 0, 0, 0, 0, 0, 0)
+
+        for(i in 1..1_000_000) {
+            countersArr[Random.nextInt(1, 7)]++
+        }
+        for (i in 1..countersArr.size-1){
+            println("MY DICE | $i is ${countersArr[i]}")
+        }
     }
 }
